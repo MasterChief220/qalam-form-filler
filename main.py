588e7d67ba4f16426e2f3c12a9fe9f2be0c9fed0
Master Chief <bassamkhanpakistan99@gmail.com>
2023-11-10 17:41:21 +0500

    Added additional code to go to the desired form

--- main.py
+++ main.py
@@ -11,17 +11,18 @@ driver.get("https://qalam.nust.edu.pk/")
 
 title = driver.title 
 driver.implicitly_wait(3)  
-#text_box = driver.find_element(by=By.NAME, value="login") 
-isvisible = driver.find_element(By.NAME, "login").is_displayed() 
-print(isvisible) 
+
 text_box = driver.find_element(by=By.NAME, value="login")
-text_box.send_keys("mbassam.mts42ceme") 
+text_box.send_keys("") # Enter your login id
 text_box = driver.find_element(by=By.NAME, value="password")
-text_box.send_keys("BK@nust#123") 
+text_box.send_keys("") # Enter your password
 login_button = driver.find_element(By.CSS_SELECTOR, "button.btn.btn-nust.btn-block.py-3.mt-4")  # Adjust the CSS selector to match the HTML attribute of the login button 
 login_button.click()
 print(driver.current_url) 
 
 wait = WebDriverWait(driver, 10)
-driver.find_element(By.PARTIAL_LINK_TEXT, "color_input").click()
-  
+#driver.find_element(By.PARTIAL_LINK_TEXT, "student/evaluation/form").click()
+tab = wait.until(EC.element_to_be_clickable((By.XPATH, '//a[contains(@href, "/student/evaluation/form")][1]')))
+tab.click()
+new_window = driver.window_handles[1]
+driver.switch_to.window(new_window)
